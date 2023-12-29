b21faf823aa481e040dcbbcc49545152a4f1e7ae
KhimmPhunxx <vakhimim1107@gmail.com>
2023-12-29 23:10:42 +0700

    second commit

---
+++ JavaScript/loading.js
@@ -0,0 +1,10 @@
+// 
+window.addEventListener("load", () => {
+    const laoder = document.querySelector(".loader");
+
+    laoder.classList.add("loader-hidden");
+
+    laoder.addEventListener("transitionend", () => {
+        document.body.removeChild("loader");
+    })
+})
